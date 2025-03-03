377c4c2173d571769e7632801713dfac785e746d
aweirdocc <aweirdocc@163.com>
2025-03-03 09:24:10 +0800

    update(): docs

---
+++ docs/blogs/2025/3d-pie.md
@@ -0,0 +1,35 @@
+---
+title: 2025
+titleTemplate: 三维饼图制作
+description: 
+createDate: 2025-02-28
+tag: 工具  
+---
+
+# 三维饼图制作
+
+## 依赖库
+
+- [echarts](https://echarts.apache.org/zh/index.html)
+- [echarts-gl](https://github.com/ecomfe/echarts-gl)
+
+## 示例
+
+<div class="chart-bar">
+  <stack projectId="vitejs-vite-8kyhm4gd"/>
+</div>
+
+<script setup>
+  import stack from '@blog/stack';
+</script>
+
+<style lang="scss">
+  .chart-bar {
+    width: 100%;
+    height: 500px;
+
+    iframe {
+      height: 450px;
+    }
+  }
+</style>
---
+++ packages/stackPreview/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="stack-previewer"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import sdk from '@stackblitz/sdk';
+
+const props = defineProps({
+  projectId: {
+    type: String,
+    required: true
+  }
+});
+
+function setupPreviewer() {
+  try{
+    if (!props.projectId) {return false}
+
+    sdk.embedProjectId('stack-previewer', props.projectId, {
+      forceEmbedLayout: true,
+      view: 'preview',
+      showSidebar: false,
+      hideExplorer: true,
+      hideNavigation: true
+    })
+  }catch(err) {
+    console.log(err)
+  }
+}
+
+onMounted(() => {
+  setupPreviewer()
+})
+</script>
+
+<style lang="scss" scoped>
+#stack-previewer {
+  width: 100%;
+  height: 100%;
+}
+</style>
